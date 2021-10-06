74ca7f3e88f475bc4fa14e1c418aa3d7296f94b9
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-06 10:38:33 -0500

    added comments

--- lottery.py
+++ lottery.py
@@ -1,12 +1,15 @@
 from random import choice
 
+"""Initial lottery choices and winning ticket"""
 lottery = ['J', 7, 'K', 88, 'L', 'B', 34, 53, 14, 'S', 20, 92, 78, 11, 49]
 ticket = []
 
+"""Create winning ticket with 5 characters"""
 while len(ticket) < 5:
     winning = choice(lottery)
 
     if winning not in ticket:
         ticket.append(winning)
+        
 print("Winning ticket:")
 print(ticket)
